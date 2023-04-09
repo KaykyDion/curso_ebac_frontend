449111eeeae74741107b61d32e04d2330a162dcb
Kayky Dion <kayky.dion@gmail.com>
2023-04-08 22:02:29 -0300

    Verificação de números

--- exercicioDom/main.js
+++ exercicioDom/main.js
@@ -9,16 +9,16 @@ form.addEventListener("submit", function (e) {
   const errorMessage = document.querySelector(".error-message");
   const warningMessage = document.querySelector(".warning-message");
 
-  const verifiedTxt = `Verificação completa: ${fieldA.value} é maior que ${fieldB.value}`;
-  const errorTxt = `Erro: ${fieldA.value} é menor que ${fieldB.value}`;
+  const verifiedTxt = `Verificação completa: ${fieldB.value} é maior que ${fieldA.value}`;
+  const errorTxt = `Erro: ${fieldB.value} é menor que ${fieldA.value}`;
   const warningTxt = `Erro: valores dos campos são iguais`;
 
-  if (fieldA.value > fieldB.value) {
+  if (fieldB.value > fieldA.value) {
     verifiedMessage.innerHTML = verifiedTxt;
     verifiedMessage.style.display = "block";
     errorMessage.style.display = "none";
     warningMessage.style.display = "none";
-  } else if (fieldB.value > fieldA.value) {
+  } else if (fieldA.value > fieldB.value) {
     errorMessage.innerHTML = errorTxt;
     errorMessage.style.display = "block";
     verifiedMessage.style.display = "none";
